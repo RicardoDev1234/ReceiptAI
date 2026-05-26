fc3bc87a449f0605f747f72a889bb439c151527e
RicardoDev1234 <ricardohatoum4@gmail.com>
2026-05-27 00:35:19 +0300

    Bump SW cache version to force fresh install

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receiptai-v1';
+const CACHE = 'receiptai-v2';
 const STATIC = ['/','index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install', e => {
